719bc26769500e4569cbebd9391e30fd71143fa8
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-12 11:46:55 -0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+	<footer class="footer">
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<p class="copyright">Copyright &copy <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
+	<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ inc/intro-home.php
@@ -0,0 +1,65 @@
+<div class="container-fluid container-padding mountaintarget">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-7">
+
+			</div>
+			<div class="col-sm-5 target-text">
+				<h2 style="color:#4c7abd;">Targeted Messaging and Strategy</h2>
+				<p>Web design changed the world in ways that we never could have imagined.</p>
+				<p>Not all businesses are fortunate enough to get built-in foot traffic and visibility. Web design changed all that and gave everyone the ability to have the same kind of visibility. It’s like having a sign that the entire world can see 24 hours a day.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid container-padding" style="background:#eaeaea;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<h2 style="color:#4c7abd;">Conversion-Focused Design</h2>
+				<p>Web design changed the world in ways that we never could have imagined.</p>
+				<p>Not all businesses are fortunate enough to get built-in foot traffic and visibility. Web design changed all that and gave everyone the ability to have the same kind of visibility. It’s like having a sign that the entire world can see 24 hours a day.</p>
+			</div>
+			<div class="col-sm-6">
+				<div class="inner">
+					<img src="/wp-content/uploads/conversion.png" class="img-fluid">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid container-padding" style="background:#fff;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="inner">
+					<img src="/wp-content/uploads/computer-infographic.png" class="img-fluid">
+				</div>
+				
+			</div>
+			<div class="col-sm-6">
+				<h2 style="color:#4c7abd;">Mobile-First Development</h2>
+				<p>Web design changed the world in ways that we never could have imagined.</p>
+				<p>Not all businesses are fortunate enough to get built-in foot traffic and visibility. Web design changed all that and gave everyone the ability to have the same kind of visibility. It’s like having a sign that the entire world can see 24 hours a day.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid container-padding" style="background:#eaeaea;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<h2 style="color:#4c7abd;">Customer Search Intent</h2>
+				<p>Web design changed the world in ways that we never could have imagined.</p>
+				<p>Not all businesses are fortunate enough to get built-in foot traffic and visibility. Web design changed all that and gave everyone the ability to have the same kind of visibility. It’s like having a sign that the entire world can see 24 hours a day.</p>
+			</div>
+			<div class="col-sm-6">
+				<div class="inner">
+					<img src="/wp-content/uploads/search.png" class="img-fluid">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
